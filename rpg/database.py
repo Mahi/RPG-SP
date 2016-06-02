92b6777184a95e7a07720089e53d936c33822f4b
Markus Meskanen <markusmeskanen@gmail.com>
2016-06-02 11:23:39 +0300

    Added table creation into Database.__init__
    For `players` and `skills` tables

--- rpg/database.py
+++ rpg/database.py
@@ -11,6 +11,21 @@ class Database:
             Path to the database file or ``':memory:'`` for RAM
         """
         self._connection = sqlite3.connect(path)
+        self._connection.execute('''
+            CREATE TABLE IF NOT EXISTS players (
+                steamid TEXT PRIMARY KEY NOT NULL,
+                level INTEGER NOT NULL,
+                xp INTEGER NOT NULL,
+                credits INTEGER NOT NULL
+            )''')
+        self._connection.execute('''
+            CREATE TABLE IF NOT EXISTS skills (
+                steamid TEXT NOT NULL,
+                class_id TEXT NOT NULL,
+                level INTEGER NOT NULL,
+                FOREIGN KEY (steamid) REFERENCES players(steamid),
+                PRIMARY KEY (steamid, class_id)
+            )''')
 
     def close(self):
         """Close the connection to the database."""
