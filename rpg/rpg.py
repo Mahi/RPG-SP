14e2f1b8887574792fd87bc97ee6fa09e18e4664
Markus Meskanen <markusmeskanen@gmail.com>
2016-06-14 14:54:25 +0300

    Added database functionality into main module

--- rpg/rpg.py
+++ rpg/rpg.py
@@ -1,6 +1,10 @@
 from event import Event
+from paths import PLUGIN_DATA_PATH
 from players.dictionary import PlayerDictionary
+from players.helpers import index_from_userid
+from players.helpers import playerinfo_from_index
 
+import rpg.database
 import rpg.player
 import rpg.skill
 import rpg.skills
@@ -17,14 +21,44 @@ def _new_player(index):
     :returns rpg.player.Player:
         The RPG player entity
     """
-    player = rpg.player.Player(index)
-    player.skills.extend(skill() for skill in _skill_classes)
+    steamid = playerinfo_from_index(index).steamid
+    player_data = _database.load_player_data(steamid)
+    player = rpg.player.Player(index, *player_data)
+    for skill_cls in _skill_classes:
+        skill_data = _database.load_skill_data(steamid, skill_cls.class_id)
+        player.skills.append(skill_cls(*skill_data))
     return player
 
 
 # Globals
+_database = rpg.database.Database(PLUGIN_DATA_PATH / 'rpg.db')
 _players = PlayerDictionary(_new_player)
 _skill_classes = list(rpg.utils.get_subclasses(rpg.skill.Skill))
+
+
+def _save_player_data(player):
+    """Save player's RPG data into the database."""
+    _database.save_player_data(player.steamid, player.level, player.xp, player.credits)
+    for skill in player.skills:
+        _database.save_skill_data(player.steamid, skill.class_id, skill.level)
+
+
+def unload():
+    """Store players' data and close the the database."""
+    for player in _players.values():
+        _save_player_data(player)
+    _database.commit()
+    _database.close()
+
+
+@Event('player_disconnect', 'player_spawn')
+def _save_player_data(event):
+    """Save player's RPG data into the database."""
+    index = index_from_userid(event['userid'])
+    if index not in _players:
+        return
+    _save_player_data(_players[index])
+    _database.commit()
 
 
 @Event('player_jump', 'player_spawn')
