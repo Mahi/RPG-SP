5a2850eb2a9574cc8e1d028a7f8fa795ac99e595
Markus Meskanen <markusmeskanen@gmail.com>
2016-06-14 14:04:02 +0300

    Added basic event functionality into main module
    Added support for the following events in skills: player_spawn,
    player_jump, player_attack, player_victim, player_kill, player_defend

--- rpg/rpg.py
+++ rpg/rpg.py
@@ -1,3 +1,4 @@
+from event import Event
 from players.dictionary import PlayerDictionary
 
 import rpg.player
@@ -24,3 +25,38 @@ def _new_player(index):
 # Globals
 _players = PlayerDictionary(_new_player)
 _skills = list(rpg.utils.get_subclasses(rpg.skill.Skill))
+
+
+@Event('player_jump', 'player_spawn')
+def _execute_independent_skill_callbacks(event):
+    """Execute skill callbacks for events with only one user.
+
+    Also makes sure the player is in a valid team to prevent
+    accidental errors with spectators and unassigned players.
+    """
+    eargs = event.variables.as_dict()
+    player = _players.from_userid(eargs.pop('userid'))
+    if player.team not in (2, 3):
+        return
+    player.execute_skill_callbacks(event.name, player=player, **eargs)
+
+
+_event_names = {
+    # event.name: (attacker_event_name, victime_event_name),
+    'player_death': ('player_kill', 'player_death'),
+    'player_hurt': ('player_attack', 'player_victim'),
+}
+
+@Event('player_death', 'player_hurt')
+def _execute_interaction_skill_callbacks(event):
+    """Execute skill callbacks for events with multiple participants."""
+    eargs = event.variables.as_dict()
+    attacker = _players.from_userid(eargs.pop('attacker'))
+    victim = _players.from_userid(eargs.pop('userid'))
+    if attacker is None or attacker == victim:
+        return
+    eargs.update(attacker=attacker, victim=victim)
+    attacker.execute_skill_callbacks(
+        _event_names[event.name][0], player=attacker, **eargs)
+    victim.execute_skill_callbacks(
+        _event_names[event.name][1], player=victim, **eargs)
